225be3650b1e2d6c14d6db8c4110ae550d060dee
Jack-PC <fjfqhwk@qq.com>
2018-04-20 19:30:54 +0800

    完成项目配置

--- src/main/java/com/fox/controller/MainController.java
+++ src/main/java/com/fox/controller/MainController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/home")
 public class MainController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = {"", "/", "view"}, method = RequestMethod.GET)
     public String index() {
-        return "index";
+        return "first";
     }
 }
