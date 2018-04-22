16c174015c382f40785c4933e40c277781f07293
Jack-PC <fjfqhwk@qq.com>
2018-04-22 18:48:49 +0800

    更新

--- src/main/java/com/fox/controller/LoginController.java
+++ src/main/java/com/fox/controller/LoginController.java
@@ -1,4 +1,34 @@
 package com.fox.controller;
 
-public class LoginController {
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginController extends AbstractController {
+    //成功与失败字段
+    private String successView;
+    private String failView;
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return null;
+    }
+
+    public String getSuccessView() {
+        return successView;
+    }
+
+    public void setSuccessView(String successView) {
+        this.successView = successView;
+    }
+
+    public String getFailView() {
+        return failView;
+    }
+
+    public void setFailView(String failView) {
+        this.failView = failView;
+    }
 }
